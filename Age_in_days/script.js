cc7de20fb3ba825d395eff73f8dd81f1b2448156
unknown <gurpreetsingh05222@gmail.com>
2020-02-29 08:23:44 +0530

    javascript logic for age in days

--- Age_in_days/script.js
+++ Age_in_days/script.js
@@ -1,1 +1,9 @@
-console.log('Hi there!');
+function ageInDays(){
+    let birthYear = prompt('What year were you born in?');
+    let ageInDayss = (2020 - birthYear) * 365;
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode('You are' + ageInDayss + 'days old');
+    h1.setAttribute('id', 'ageInDayss');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
